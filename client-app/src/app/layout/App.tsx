5ed966da8f7de5c6a65257087f3e71352fe630d4
Lastingyeh <yuhung.yeh@advantech.com.tw>
2020-07-15 19:02:30 +0800

    + client-app activity

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, Fragment } from "react";
+import { Container } from "semantic-ui-react";
+import axios from "axios";
+import { IActivity } from "../models/activity";
+import NavBar from "../../features/nav/NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+
+const App = () => {
+    const [activities, setActivities] = useState<IActivity[]>([]);
+    const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+        null
+    );
+
+    const [editMode, setEditMode] = useState(false);
+
+    const handleSelectActivity = (id: string) => {
+        setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+    };
+
+    const handleOpenCreateForm = () => {
+        setSelectedActivity(null);
+        setEditMode(true);
+    }
+
+    useEffect(() => {
+        axios
+            .get<IActivity[]>("http://localhost:5000/api/activities")
+            .then((response) => {
+                setActivities(response.data);
+            });
+    }, []);
+
+    return (
+        <Fragment>
+            <NavBar openCreateForm={handleOpenCreateForm}/>
+            <Container style={{ marginTop: "7em" }}>
+                <ActivityDashboard
+                    activities={activities}
+                    selectActivity={handleSelectActivity}
+                    selectedActivity={selectedActivity}
+                    editMode={editMode}
+                    setEditMode={setEditMode}
+                    setSelectedActivity={setSelectedActivity}
+                />
+            </Container>
+        </Fragment>
+    );
+};
+
+export default App;
